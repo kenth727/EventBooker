86aefa9a731fe24e145fafacfd4ec088f6a9f5fe
kent.huang <kenthuang727@gmail.com>
2025-07-25 18:00:29 -0700

    - implement jwt middleware token - implement token checking for endpoints

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -3,7 +3,10 @@ const Booking = require("../../models/booking");
 const { transformBooking, transformEvent } = require("./merge");
 
 module.exports = {
-  bookings: async () => {
+  bookings: async (args, req) => {
+    if(!req.isAuth) {
+        throw new Error('Unauthenticated');
+    }
     try {
       const bookings = await Booking.find();
       return bookings.map(booking => {
@@ -11,16 +14,22 @@ module.exports = {
       });
     } catch (err) {}
   },
-  bookEvent: async (args) => {
+  bookEvent: async (args, req) => {
+    if(!req.isAuth) {
+        throw new Error('Unauthenticated');
+    }
     const fetchedEvent = await Event.findOne({ _id: args.eventId });
     const booking = new Booking({
-      user: "687dee80b686c6091a2deb62",
+      user: req.userId,
       event: fetchedEvent,
     });
     const result = await booking.save();
     return transformBooking(result);
   },
-  cancelBooking: async (args) => {
+  cancelBooking: async (args, req) => {
+    if(!req.isAuth) {
+        throw new Error('Unauthenticated');
+    }
     try {
       const booking = await Booking.findById(args.bookingId).populate("event");
       const event = transformEvent(booking.event);
