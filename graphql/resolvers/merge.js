7939da4991053a4a4eef56b4132e8f0c99254cea
kent.huang <kenthuang727@gmail.com>
2025-07-27 14:12:43 -0700

    - return events from data loader in sorted order

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,22 +1,27 @@
-const DataLoader = require('dataloader');
+const DataLoader = require("dataloader");
 
 const Event = require("../../models/event");
 const User = require("../../models/user");
-const {dateToString} = require('../../helpers/date');
+const { dateToString } = require("../../helpers/date");
 
 const eventLoader = new DataLoader((eventIds) => {
-    return events(eventIds);
+  return events(eventIds);
 });
 
 const userLoader = new DataLoader((userIds) => {
-    return User.find({_id: {$in: userIds}});
+  return User.find({ _id: { $in: userIds } });
 });
 
 const events = async (eventIds) => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
-    return events.map(event => {
-      return transformEvent(event)
+    events.sort((a, b) => {
+      return (
+        eventIds.indexOf(a._id.toString()) - eventIds.indexOf(b._id.toString())
+      );
+    });
+    return events.map((event) => {
+      return transformEvent(event);
     });
   } catch (err) {
     throw err;
@@ -54,15 +59,15 @@ const transformEvent = (event) => {
   };
 };
 
-const transformBooking = booking => {
-    return {
-        ...booking._doc,
-        _id: booking.id,
-        user: user.bind(this, booking._doc.user),
-        event: singleEvent.bind(this, booking._doc.event),
-        createdAt: dateToString(booking._doc.createdAt),
-        updatedAt: dateToString(booking._doc.updatedAt),
-    };
+const transformBooking = (booking) => {
+  return {
+    ...booking._doc,
+    _id: booking.id,
+    user: user.bind(this, booking._doc.user),
+    event: singleEvent.bind(this, booking._doc.event),
+    createdAt: dateToString(booking._doc.createdAt),
+    updatedAt: dateToString(booking._doc.updatedAt),
+  };
 };
 
 exports.transformEvent = transformEvent;
@@ -71,4 +76,3 @@ exports.transformBooking = transformBooking;
 // exports.user = user;
 // exports.events = events;
 // exports.singleEvent = singleEvent;
-
